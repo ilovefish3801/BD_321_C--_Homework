fd701e263d3b25a581aa0c9907203fa9e35fba57
kurulo kezemiakin <138194455+ilovefish3801@users.noreply.github.com>
2024-01-14 13:52:16 +0200

    Create Homework.cpp

---
+++ Homework.cpp
@@ -0,0 +1,26 @@
+#include <iostream>
+#include "Task1.h"
+#include "Task2.h"
+#include "Task3.h"
+
+using namespace std;
+
+int main()
+{
+            // Task 1
+    /*cout << Task1_m << endl;
+    removeInBrackets(Task1_m);
+    cout << Task1_m << endl;*/
+
+            // Task 2
+    /*cout << "Enter the text: ";
+    cin >> Task2_m;
+    countPairs(Task2_m);*/
+
+            // Task 3
+    cout << Task3_m << endl;
+    removeWordsUnderThree(Task3_m);
+    cout << Task3_m << endl;
+
+    return 0;
+}
