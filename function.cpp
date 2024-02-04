42a5634fecf62db074ba3b3d09fa30d84a9f2d1d
kurulo kezemiakin <138194455+ilovefish3801@users.noreply.github.com>
2024-02-04 14:36:30 +0200

    Update function.cpp

--- function.cpp
+++ function.cpp
@@ -7,25 +7,48 @@ int getRandomNumber(int min, int max) {
     return rand() % (max - min) + min;
 };
 
+double getRandomDouble(double min, double max) {
+    double number = ((double)rand() / RAND_MAX) * (max - min) + min;
+
+    return round(number * 10) / 10;
+}
+
 char getRandomSymbol() {
-    const char symbols[26] = { '!', '@', '#', '$', '%', '^', '&', '*', '(', ')', '-', '+', '=', '{', '}', '[', ']', '|', ';', ':', '<', '>', '?', '/', '.', ',' };
+    const char symbols[22] = { '!', '@', '#', '$', '%', '^', '&', '*', '(', ')', '-', '+', '=', '{', '}', '[', ']', '|', '<', '>', '?', '/' };
 
     int index = rand() % (sizeof(symbols) / sizeof(symbols[0]));
 
     return symbols[index];
 };
 
-void fillArray(Type*& arr, const int size) {
+void fillArrayNumbers(int* arr, const int size) {
     for (int i = 0; i < size; i++) {
         arr[i] = getRandomNumber(-9, 9);
     }
 };
 
-void printArray(Type*& arr, const int size) {
+void fillArrayDoubles(double* arr, const int size) {
+    for (int i = 0; i < size; i++) {
+        arr[i] = getRandomDouble(-9.9, 9.9);
+    }
+};
+
+void fillArraySymbols(char* arr, const int size) {
+    for (int i = 0; i < size; i++) {
+        arr[i] = getRandomSymbol();
+    }
+};
+
+void printArray(Type* arr, const int size) {
     cout << "Array: " << endl;
 
     for (int i = 0; i < size; i++) {
-        cout << arr[i] << ", ";
+        if (i != size - 1) {
+            cout << arr[i] << ", ";
+        }
+        else {
+            cout << arr[i] << ".";
+        }
     }
 
     cout << endl;
