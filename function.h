4907634a37d9ae020b21e75e3b324a0593a45152
kurulo kezemiakin <138194455+ilovefish3801@users.noreply.github.com>
2024-02-04 14:36:41 +0200

    Update function.h

--- function.h
+++ function.h
@@ -1,14 +1,28 @@
+#pragma once
+
+#define DOUBLE
+
 #ifdef INTEGER
 typedef int Type;
-#define ShowType ShowInt
+#define FILL_ARRAY fillArrayNumbers
+#define PRINT_ARRAY printArray
+
 #elif defined DOUBLE
 typedef double Type;
-#define ShowType ShowDouble
+#define FILL_ARRAY fillArrayDoubles
+#define PRINT_ARRAY printArray
+
 #elif defined CHAR
 typedef char Type;
+#define FILL_ARRAY fillArraySymbols
+#define PRINT_ARRAY printArray
+
 #endif
 
 int getRandomNumber(int min, int max);
+double getRandomDouble(double min, double max);
 char getRandomSymbol();
-void fillArray(Type*& arr, const int size);
-void printArray(Type*& arr, const int size);
+void fillArrayNumbers(int* arr, const int size);
+void fillArrayDoubles(double* arr, const int size);
+void fillArraySymbols(char* arr, const int size);
+void printArray(Type* arr, const int size);
