74b4702d8b0e97887a11faac127e1f787a34c23d
kurulo kezemiakin <138194455+ilovefish3801@users.noreply.github.com>
2024-02-04 14:36:54 +0200

    Update prog.cpp

--- prog.cpp
+++ prog.cpp
@@ -1,11 +1,17 @@
 #include <iostream>
-#include "function.cpp"
+#include "function.h"
 
 using namespace std;
 
-#define INTEGER
+// В мене трохи дивно вийшло, але суть передав. #define INTEGER, DOUBLE або CHAR треба писати у function.h, бо якщо тут, то не передається
 int main() {
 
+	srand(time(nullptr));
+
+	const int size = 10;
+	Type arr[size];
+	::FILL_ARRAY(arr, size);
+	::PRINT_ARRAY(arr, size);
 
 	return 0;
 }
